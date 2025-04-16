9d09ac390ae63bbc3cdeef922278a92573985c5e
hyungjae.lee <dlgudwo11@gmail.com>
2025-04-16 15:42:26 +0900

    Kafka 선착순 이벤트 Init Commit

---
+++ src/main/java/com/hj/kafkaeventrush/KafkaEventrushApplication.java
@@ -0,0 +1,13 @@
+package com.hj.kafkaeventrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEventrushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEventrushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hj/kafkaeventrush/KafkaEventrushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.kafkaeventrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEventrushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
