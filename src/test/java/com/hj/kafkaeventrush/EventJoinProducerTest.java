917b24a324dc20c27a1b2223f55ffc11c0bb9189
HJ <31768616+devHjlee@users.noreply.github.com>
2025-04-18 08:02:40 +0900

    test(produce): 파티셔너 전략별 파티션 분산 테스트 코드 추가 (#3)
    - 유저_멀티스레드전송
    - stickyVSroundrobinTest를 통해 파티션 RoundRobin, Sticky 전략별 데이터 전송 확인

--- src/test/java/com/hj/kafkaeventrush/EventJoinProducerTest.java
+++ src/test/java/com/hj/kafkaeventrush/EventJoinProducerTest.java
@@ -1,12 +1,15 @@
 package com.hj.kafkaeventrush;
 
 import com.hj.kafkaeventrush.dto.EventJoinMessage;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,22 +20,46 @@ public class EventJoinProducerTest {
     private KafkaTemplate<String, EventJoinMessage> eventJoinKafkaTemplate;
 
     @Test
-    void 유저_100명_멀티스레드전송() {
+    void 유저_멀티스레드전송() throws InterruptedException {
         int threadCount = 10;
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
 
-        for (long i = 0; i < 100; i++) {
+        for (long i = 0; i < 300; i++) {
             long userId = i;
             executor.submit(() -> {
                 EventJoinMessage message = new EventJoinMessage(userId, LocalDateTime.now());
-                eventJoinKafkaTemplate.send("event.join", message);
-                System.out.printf("[Thread: %s] 전송 완료 userId=%d%n", Thread.currentThread().getName(), userId);
+                SendResult<String, EventJoinMessage> result = null;
+                try {
+                    result = eventJoinKafkaTemplate.send("event.join", message).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("userId=%d → partition=%d%n", userId, result.getRecordMetadata().partition());
             });
         }
 
         // 전송 완료 대기
-        executor.shutdown();
+        //executor.shutdown();
+        Thread.sleep(5000);
 
         System.out.println("100명 유저 메시지 멀티스레드 전송 완료");
+    }
+
+    @Test
+    void stickyVSroundrobinTest() throws InterruptedException, ExecutionException {
+        // KafkaProducerConfig 설정 추가 필요
+        // config.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "org.apache.kafka.clients.producer.RoundRobinPartitioner");
+        // config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        for (long i = 0; i < 300; i++) {
+            long userId = i;
+            EventJoinMessage message = new EventJoinMessage(userId, LocalDateTime.now());
+            SendResult<String, EventJoinMessage> result = eventJoinKafkaTemplate.send("event.join", message).get();
+            System.out.printf("userId=%d → partition=%d%n", userId, result.getRecordMetadata().partition());
+        }
+
+        // 전송 완료 대기
+        Thread.sleep(5000);
+
+        System.out.println("stickyVSroundrobinTest 완료");
     }
 }
